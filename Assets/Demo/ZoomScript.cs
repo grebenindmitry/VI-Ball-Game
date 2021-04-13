9f9d76611ce444e12d410ef07f42576a65e9ef45
Dmitry Grebenin <grebenin.dmitry@gmail.com>
2021-04-13 15:41:44 +0300

    Attempts at zoom

--- Assets/Demo/ZoomScript.cs
+++ Assets/Demo/ZoomScript.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class ZoomScript : MonoBehaviour
 {
+    public Image image;
+    
+    private bool _isZoomed = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,37 @@ public class ZoomScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ZoomToggle()
+    {
+        if (_isZoomed)
+        {
+            gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(2f, 2f, 2f);
+        }
+
+        _isZoomed = !_isZoomed;
+    }
+
+    private void OnPostRender()
+    {
+        var width = Screen.width / 2;
+        var height = Screen.height / 2;
+        var startX = Screen.width / 4;
+        var startY = Screen.height / 4;
+        var tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+        Rect rex = new Rect(startX, startY, width, height);
+     
+        tex.ReadPixels(rex, 0, 0);
+        tex.Apply();
+
+        image.sprite = Sprite.Create(tex, new Rect(0, 0, width, height), Vector2.zero);
+
+        Destroy(tex);
     }
 }
