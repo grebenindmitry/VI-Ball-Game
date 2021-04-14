aba8992cbbf32e1f8cf73e5a3f9ca5db2050d530
Dmitry Grebenin <grebenin.dmitry@gmail.com>
2021-04-14 21:40:50 +0300

    Reorganization of files, implementation of proximity warnings

---
+++ Assets/Scripts/TextToSpeechScript.cs
@@ -0,0 +1,55 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace CO2403
+{
+    public class TextToSpeechScript : MonoBehaviour
+    {
+        private AndroidJavaObject _unityActivity;
+        private AndroidJavaObject _unityTextToSpeech;
+
+        private void Awake()
+        {
+            _unityActivity =
+                new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+            _unityTextToSpeech =
+                new AndroidJavaObject("io.github.grebenindmitry.texttospeech.UnityTextToSpeech", _unityActivity);
+        }
+
+        private void ShowToast(string message)
+        {
+            var toastClass = new AndroidJavaClass("android.widget.Toast");
+            _unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                var toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", _unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+
+        private void NativeTts(string message)
+        {
+            _unityTextToSpeech.Call("Speak", message);
+        }
+
+        public void SpeakText(string text)
+        {
+            NativeTts(text);
+        }
+
+        public void SpeakText(TextMeshProUGUI textBox)
+        {
+            NativeTts(textBox.text);
+        }
+
+        public void Stop()
+        {
+            _unityTextToSpeech.Call("Stop");
+        }
+        
+        private void OnDestroy()
+        {
+            _unityTextToSpeech.Call("Stop");
+        }
+    }
+}
