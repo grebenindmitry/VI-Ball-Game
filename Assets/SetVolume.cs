14e2fc2e9847647f0ad4bc11535f8435abf57580
DimitrisNitsios <dnitsios@uclan.ac.uk>
2021-04-14 18:32:57 +0300

    changes

--- Assets/SetVolume.cs
+++ Assets/SetVolume.cs
@@ -13,9 +13,10 @@ public class SetVolume : MonoBehaviour {
     {
         slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
     }
+
     public void SetLevel (float sliderValue)
     {
-	mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+	    mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MusicVolume", sliderValue);
     }
 }
