2ed2ff191b5d8b7e175a0d2db1f808e6a6c67836
Dmitry Grebenin <grebenin.dmitry@gmail.com>
2021-04-15 22:15:09 +0300

    simplify code

--- Assets/Scripts/HowToAudioScript.cs
+++ Assets/Scripts/HowToAudioScript.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 public class HowToAudioScript : MonoBehaviour
 {
 
-    public TextMeshProUGUI message;
+    public TMP_Text message;
 
     // Start is called before the first frame update
     void Start()
     {
         var tts = GetComponent<TextToSpeechScript>();
-        tts.SpeakText(message);
+        tts.SpeakText(message.text);
     }
 }
