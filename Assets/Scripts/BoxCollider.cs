022d5502f7fc47f005cfce9f9bf349c9c172dd7b
DimitrisNitsios <dnitsios@uclan.ac.uk>
2021-04-16 12:45:58 +0300

    comments

--- Assets/Scripts/BoxCollider.cs
+++ Assets/Scripts/BoxCollider.cs
@@ -5,18 +5,20 @@ public class BoxCollider : MonoBehaviour
 {
 
     public GameObject endCanvas;
-    private TextToSpeechScript _tts;
-       
+    private TextToSpeechScript _tts;       
 
     private void Awake()
     {
         _tts = gameObject.AddComponent<TextToSpeechScript>();
     }
 
+    // Method that resolves collitions
     private void OnCollisionEnter(Collision collision)
     {       
+        // anythig that collider with the boxes collider looses its gravity
         collision.rigidbody.useGravity = false;
         
+        //activate canvas and sent audio message to player
         endCanvas.SetActive(true);
         _tts.SpeakText(endCanvas.GetComponentInChildren<TMP_Text>().text);
     }
