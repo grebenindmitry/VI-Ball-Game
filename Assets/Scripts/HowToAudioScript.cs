feac859ab6d60a0ddbb98b668e205b23c5be7963
SZacharia <SZacharia@uclan.ac.uk>
2021-04-16 14:42:51 +0300

    Minor changes bug fixes

--- Assets/Scripts/HowToAudioScript.cs
+++ Assets/Scripts/HowToAudioScript.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+
 public class HowToAudioScript : MonoBehaviour
 {
 
     public TMP_Text message;
+    private TextToSpeechScript _tts;
 
     // Start is called before the first frame update
     private void Start()
     {
-        //Applies the how-to text to Text-To-Speech
-        var tts = GetComponent<TextToSpeechScript>();
-        tts.SpeakText(message.text);
+        _tts = GetComponent<TextToSpeechScript>();
+        StartCoroutine(StartTts());
+    }
+
+    //Wait for .5 seconds (to allow tts to init) and start speaking
+    private IEnumerator StartTts()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _tts.SpeakText(message.text);
     }
 }
